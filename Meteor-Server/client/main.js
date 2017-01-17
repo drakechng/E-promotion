193d028ef404be8760d9ef711ac7272a9543e2e2
Freeman.Xiong <xiongchenyu6@gmail.com>
2017-01-17 16:45:37 +0800

    Modify

--- Meteor-Server/client/main.js
+++ Meteor-Server/client/main.js
@@ -1,5 +1,3 @@
-
-import './main.html';
 import './main.css'
 import '/imports/startup/client';
 import '../imports/startup/accounts-config.js';
---
+++ Meteor-Server/imports/ui/Layouts/AsideBar.jsx
@@ -0,0 +1,81 @@
+import React, {Component, PropTypes} from 'react';
+
+
+export default class AsideBar extends Component {
+   render(){
+return(
+      <aside className="main-sidebar">
+         <section className="sidebar">
+            <div className="user-panel">
+               <div className="pull-left image">
+                  <img src="/img/user2-160x160.jpg" className="img-circle" alt="User Image"/>
+               </div>
+               <div className="pull-left info">
+                  <p>Alexander Pierce</p>
+                  <a href="#"><i className="fa fa-circle text-success"></i> Online</a>
+               </div>
+            </div>
+            <form action="#" method="get" className="sidebar-form">
+               <div className="input-group">
+                  <input type="text" name="q" className="form-control" placeholder="Search..."/>
+              <span className="input-group-btn">
+                <button type="submit" name="search" id="search-btn" className="btn btn-flat"><i className="fa fa-search"></i>
+                </button>
+              </span>
+               </div>
+            </form>
+            <ul className="sidebar-menu">
+                <li className="header">MAIN NAVIGATION</li>
+
+                  <li>
+                     <a href="index2.html">
+                        <i className="fa fa-dashboard"></i> <span>Overview</span></a></li>
+
+                  <li className="treeview">
+                     <a href="#">
+                        <i className="fa fa-plus"></i> <span>Create</span>
+                        <span className="pull-right-container">
+              <i className="fa fa-angle-left pull-right"></i>
+            </span>
+                     </a>
+                     <ul className="treeview-menu">
+                        <li><a href="pages/forms/voucher.html"><i className="fa fa-circle-o"></i>Voucher</a></li>
+                        <li className="active"><a href="pages/forms/estamp.html"><i className="fa fa-circle-o"></i>E-Stamps System</a></li>
+                        <li className="active"><a href="index2.html"><i className="fa fa-circle-o"></i>Reward Points System</a></li>
+
+
+                     </ul>
+                  </li>
+
+                  <li>
+                     <a href="index2.html">
+                        <i className="fa fa-gear"></i> <span>Manage</span></a></li>
+
+                  <li>
+                     <a href="pages/calendar.html">
+                        <i className="fa fa-calendar"></i> <span>Calendar</span>
+                        <span className="pull-right-container">
+              <small className="label pull-right bg-red">3</small>
+              <small className="label pull-right bg-blue">17</small>
+            </span>
+                     </a>
+                  </li>
+                  <li>
+                     <a href="pages/mailbox/mailbox.html">
+                        <i className="fa fa-envelope"></i> <span>Mailbox</span>
+                        <span className="pull-right-container">
+              <small className="label pull-right bg-yellow">12</small>
+              <small className="label pull-right bg-green">16</small>
+              <small className="label pull-right bg-red">5</small>
+            </span>
+                     </a>
+                  </li></ul>
+
+
+         </section>
+      </aside>)
+
+
+   }
+
+}
